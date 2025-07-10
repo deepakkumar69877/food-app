059bed00e727649abdbb0727077b4b795bb660a1
deepak kumar <deepak.k@royalcyber.com>
2025-07-10 21:16:06 +0530

    add sadcn commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div> 
-   <Button variant="outline">Button</Button>
+   <Button>Button</Button>
     </div>
   );
 }
